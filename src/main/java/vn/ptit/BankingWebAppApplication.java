f1814a2f0d73744541ed3d69dfa8db964e35d7a2
Cuong Pham <phamcuongth2000@gmail.com>
2021-11-06 09:18:48 +0700

    Initial commit

---
+++ src/main/java/vn/ptit/BankingWebAppApplication.java
@@ -0,0 +1,13 @@
+package vn.ptit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingWebAppApplication.class, args);
+	}
+
+}
