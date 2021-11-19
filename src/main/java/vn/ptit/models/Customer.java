05f0db9867466d826e89aa3133b9394db2f89d0f
Lê Đức Phan Anh <leducphananh@gmail.com>
2021-11-19 15:57:07 +0700

    v1

---
+++ src/main/java/vn/ptit/models/Customer.java
@@ -0,0 +1,64 @@
+package vn.ptit.models;
+
+import java.util.Date;
+
+public class Customer {
+	private int id;
+	private String fullName;
+	private String idCard;
+	private Date dateOfBirth;
+	private String address;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String fullName, String idCard, Date dateOfBirth, String address) {
+		super();
+		this.fullName = fullName;
+		this.idCard = idCard;
+		this.dateOfBirth = dateOfBirth;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
